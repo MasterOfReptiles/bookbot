9d546c0a83cad3a50dd91b3cf7ec2451f6cdbee5
MasterOfReptiles <arozpendek13@gmail.com>
2025-12-27 19:57:48 +0100

    Finished main functionality and added report

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count, sort_dictionary
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         f.close()
         return file_contents
+    
+def print_report(path,words,characters):
+    print(f'''============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {words} total words
+--------- Character Count -------''')
+    for item in characters:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def main():
-    book_contents = get_book_text("./books/frankenstein.txt")
+    path_to_book = "./books/frankenstein.txt"
+    book_contents = get_book_text(path_to_book)
     num_words = get_word_count(book_contents)
-    print(f"Found {num_words} total words")
+    character_dictionary = get_character_count(book_contents)
+    list_of_characters = sort_dictionary(character_dictionary)
+    print_report(path_to_book,num_words,list_of_characters)
+
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,25 @@ def get_word_count(book_contents):
     word_list = book_contents.split()
     word_count = len(word_list)
     return word_count
+
+def get_character_count(book_contents):
+    book_contents_lower = book_contents.lower()
+    book_contents_withot_spaces = book_contents_lower.replace(' ','')
+    letters = {}
+    for char in book_contents_withot_spaces:
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] =1
+    return letters
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dictionary(dictionary):
+    dictionary_list = []
+    for item in dictionary:
+        dictionary_list.append({"char" : item, "num" : dictionary[item]}) 
+    dictionary_list.sort(reverse=True, key=sort_on)
+    return dictionary_list
+
